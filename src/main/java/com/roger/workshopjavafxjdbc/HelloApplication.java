3e743fe97eeeb3d93ad053fad8612c1d9a86463a
Roger Silva <karlorms@gmail.com>
2025-05-08 19:14:43 -0300

    Main view design

--- src/main/java/com/roger/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/com/roger/workshopjavafxjdbc/HelloApplication.java
@@ -3,6 +3,7 @@ package com.roger.workshopjavafxjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        Scene scene = new Scene(scrollPane);
+        //stage.setMaximized(true);
         stage.setTitle("JavaFX application");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/roger/workshopjavafxjdbc/MainView.java
+++
@@ -1,4 +0,0 @@
-package com.roger.workshopjavafxjdbc;
-
-public class MainView {
-}
